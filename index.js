a8eacbb699a14b22fcc46561712aad03aae8eff6
livolickie <havingteam@gmail.com>
2021-11-20 18:09:46 +0500

    part2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 
 const app = express()
 
@@ -11,21 +12,28 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.set("view engine", "ejs") //We will use Pug as a render engine
 
-app.route('/').get((req, res) => {
-    res.render("index", {
-        appTitle: "Labs Rafikov Danil",
+app.route('/').get((req, res) => res.redirect('/1'))
+
+app.route('/:part').get((req, res, next) => {
+    let part = req.params.part
+    if (part == 'favicon.ico') return next()
+    if (!fs.existsSync(`views/partial/part${part}.ejs`)) return res.render('404', {message: 'Work in progres...'})
+
+    res.render("_master.ejs", {
+        title: `Part ${req.params.part}`,
         helpers: {
             getRandomInt(min, max) {
                 min = Math.ceil(min);
                 max = Math.floor(max);
                 return Math.floor(Math.random() * (max - min + 1)) + min;
             }
-        }
+        },
+        renderPage: `partial/part${part}.ejs`
     })
 })
 
 app.use((req, res) => {
-    res.render("404")
+    res.render('404', {message: 'Маршрут не найден!'})
 })
 
 app.listen(PORT, () => {
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,2 +1,32 @@
 M.AutoInit() //Initialize Materialize
 AOS.init() //Initialize AOS Library for scroll animations
+
+const PAGE_COUNT = 2
+
+let page_num = -1
+
+function navUpdate() {
+    try {
+        let li_list = document.querySelectorAll('.li_link')
+        let li_arrows = document.querySelectorAll('li.arrow')
+        let splitted = document.URL.split('/')
+        page_num = Number.parseInt(splitted[splitted.length - 1]) - 1
+        li_list[page_num].classList.add('active')
+
+        if (page_num == 0) li_arrows[0].classList.add('disabled')
+        if (page_num+1 >= PAGE_COUNT) li_arrows[1].classList.add('disabled')
+
+        li_arrows[0].addEventListener('click', (eent) => {
+            if (page_num - 1 > -1) window.location.replace(`/${page_num}`)
+  
+        })
+        li_arrows[1].addEventListener('click', () => {
+            if (page_num + 1 < PAGE_COUNT) window.location.replace(`/${page_num+2}`)
+        })
+
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+navUpdate()
